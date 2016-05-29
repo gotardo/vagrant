0abaedd0dd375b7fa3328fe8e24751c82a9b5c32
gotardo <contact@gotardo.es>
2016-05-29 19:08:04 +0200

    Based on Ubuntu 16.04 / Auto install PHP7 and needed extensions

--- Vagrantfile
+++ Vagrantfile
@@ -2,20 +2,16 @@
 # vi: set ft=ruby :
 
 Vagrant.configure(2) do |config|
-	config.vm.box = "ubuntu/xenial64"
-	config.vm.synced_folder "./www/", "/var/www/",  
-		# NFS config is only valid for Linxu and MacOSX
-		type: "nfs"
-	config.vm.network :forwarded_port, host: 80, guest: 80
-    config.vm.network :forwarded_port, host: 27017, guest: 27017
-    config.vm.network :forwarded_port, host: 3606, guest: 3606
-    config.vm.network :private_network, ip: "192.168.51.10"
+	#config.vm.box = "ubuntu/xenial64"
+	#config.vm.box = "geerlingguy/ubuntu1604"
+	config.vm.box = "gbarbieru/xenial"
+	config.vm.synced_folder "./www/", "/var/www/", type: "nfs"
+	config.vm.network :private_network, ip: "192.168.51.10"
 	
-	# config.vm.provision "shell", path: "script/01-up.sh"
+	config.vm.provision "shell", path: "script/01-up.sh"
 	
 	config.vm.provider "virtualbox" do |v|
-		v.name = "php7dev"
-
+		v.name = "ubuntu-xenial"
 		v.customize [
 			"modifyvm", :id, 
 			"--memory", 2048, 
--- script/01-up.sh
+++ script/01-up.sh
@@ -1,48 +1,49 @@
 #
-#	Tested with "ubuntu/wily64"
+#	Tested with "ubuntu/xenial64"
+#	Gotardo González
 #
+
 echo "============================================="
 echo "- ¡Hola! :-)"
 echo "============================================="
 lsb_release -a
-# install main dependencies
+
 echo "============================================="
 echo "- Installing Main Dependencies"
 echo "============================================="
-sudo apt-get install -y openssl pkg-config libssl-dev libsslcommon2-dev
-sudo apt-get install -y git
 
-# install databases
+sudo apt-get install -y git curl zip unzip
+
+
 echo "============================================="
 echo "- Installing Databases"
 echo "============================================="
-sudo apt-get -y install mysql-server
+
+# sudo apt-get -y install mysql-server
 sudo apt-get -y install mongodb
 sudo apt-get -y install redis-server
 
-
-# install php dev tools
 echo "============================================="
 echo "- Installing PHP"
 echo "============================================="
-sudo apt-get -y install php7.0-fpm php7.0-cli php7.0-common 
-# install extensions
+
+sudo apt-get -y install php7.0-fpm php7.0-cli php7.0-common php7.0-dev
+
 echo "============================================="
 echo "- Installing PHP Extensions and tools"
 echo "============================================="
-sudo apt-get -y install php7.0-curl php7.0-json php7.0-opcache php7.0-mysqli 
 
-#install redis extension
-sudo pecl install mongodb;
-echo "extension = mongodb.so" | sudo tee --append /etc/php/7.0/cli/php.ini 
-echo "extension = mongodb.so" | sudo tee --append /etc/php/7.0/fpm/php.ini 
-
-#install redis extension
+sudo apt-get -y install php-mcrypt
+sudo apt-get -y install php-zip 
+sudo apt-get -y install php-curl 
+sudo apt-get -y install php-json 
+sudo apt-get -y install php-opcache
+sudo apt-get -y install php-mysqli
+sudo apt-get -y install php-xml 
+sudo apt-get -y install php-mongodb 
 sudo apt-get -y install php-redis
 
-
 # install composer from site, composer pkg from repositories have dependencies 
-# problems with php7.0
 php -r "readfile('https://getcomposer.org/installer');" > composer-setup.php
 php composer-setup.php --filename=composer
 php -r "unlink('composer-setup.php');"
@@ -56,8 +57,6 @@ sudo chmod a+x /usr/local/bin/phpunit
 sudo curl -LsS https://symfony.com/installer -o /usr/local/bin/symfony
 sudo chmod a+x /usr/local/bin/symfony
 
-
-
 echo "============================================="
 echo "- Extra dependencies"
 echo "============================================="
@@ -67,6 +66,7 @@ echo "============================================="
 echo "- Tools I like	"
 echo "============================================="
 sudo apt-get install -y nano
+sudo apt-get install -y nodejs npm 
 sudo npm install apidoc -g
 
 echo "============================================="
@@ -77,9 +77,9 @@ echo "alias project_uncache=\"php bin/console -v cache:clear\"
 alias project_run=\"project_uncache && php bin/console -v server:run 192.168.51.10:8080\"
 alias project_dependencies=\"composer update && composer install\"
 alias project_test=\"clear && php bin/console cache:clear &&  phpunit ./tests --verbose --colors\"
-" | sudo tee --append ~/.bash_profile
+" | sudo tee --append /home/vagrant/.bash_profile
 
-source ~/.bash_profile
+source /home/vagrant/.bash_profile
 sudo service php7.0-fpm restart
 
 # Done
